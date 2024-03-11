ecd60f66947568a9d0dc17e6b7c72cedb44e92f2
hettesovci <160774640+hettesovci@users.noreply.github.com>
2024-03-11 09:00:54 +0100

    Add files via upload

---
+++ js/variables.js
@@ -0,0 +1,31 @@
+/**
+ * Javascript variables
+ */
+ 
+//RTL support
+var rtl = false;                                //Set to true if your website is RTL. Otherwise, keep it false.
+
+//ONE-PAGE NAVIGATION HIGHLIGHT
+var onepage_nav = true;                         //If true, each menu item will highlight when scrolling to each respective section.
+
+//MOBILE MENU TITLE
+var mobile_menu_title = "Menu";                 //The title of the mobile menu.
+
+//HERO FULLSCREEN VARIABLE
+var hero_full_screen = true;                    //If true, the hero section will fit to screen size. If false, hero height will be the height of its content.
+
+//HERO BACKGROUND SLIDESHOW IMAGES
+// var slidehow_images = [
+//     "https://sophie.brzy-svoji.cz/wp-content/uploads/sites/60/slide-1-1920x800.jpg",
+//     "https://sophie.brzy-svoji.cz/wp-content/uploads/sites/60/slide-2-1920x800.jpg",
+//     "https://via.placeholder.com/1920x1080.png"
+// ];
+
+//TIMELINE PARALLAX
+var timeline_parallax = true;                   //If false, the timeline items will not move on mousemove.
+
+
+//CONTACT FORM VARIABLES
+var contact_form_success_msg = "Form successfully submitted! :)";
+var contact_form_error_msg = "Error sending message :(";
+var contact_form_recaptcha_error_msg = "Error verifying reCaptcha!";
